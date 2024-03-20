1a8db97479f91efa0b320a3b6fe60bf1e5641f06
vkuhnle <164048773+vkuhnle@users.noreply.github.com>
2024-03-20 09:28:30 +0100

    Update streamlit_app.py
    moved fv_df code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 # from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
@@ -30,7 +31,10 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
+    
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -45,7 +49,4 @@ if ingredients_list:
 
         st.success('Your smoothie is ordered, ' + name_on_order + '!',icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
