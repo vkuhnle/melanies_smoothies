deb435506ed8a5e4b07fce6f80bc3325a2cee574
vkuhnle <164048773+vkuhnle@users.noreply.github.com>
2024-03-20 09:30:20 +0100

    Update streamlit_app.py
    added chosen fruit to fv_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     
