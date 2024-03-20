2c995e2b048c8af18ceec857b117c152759e426e
vkuhnle <164048773+vkuhnle@users.noreply.github.com>
2024-03-20 10:55:32 +0100

    Update streamlit_app.py
    fixed a typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df=my_dataframe.to_pandas()
-st.datafrane(pd_df)
+st.dataframe(pd_df)
 
 st.stop()
 
