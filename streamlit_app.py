d0156ad65e6934caa91253f9d9238e7d40951e53
vkuhnle <164048773+vkuhnle@users.noreply.github.com>
2024-03-20 11:36:10 +0100

    Update streamlit_app.py
    added search_on to search url

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
     
